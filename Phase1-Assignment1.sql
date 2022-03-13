9125a5b85f22833040ef71ae6756d15add0b68c7
vimalprogrammer <msvimal10@gmail.com>
2022-03-13 18:23:21 +0530

    Huawei-Phase1 Assignment question added

---
+++ Phase1-Assignment1.sql
@@ -0,0 +1,10 @@
+-- Assignment 1: Create a DB for your college with following parameters:
+
+-- 1. Create a table for students with name, age, mobile no, registration no., year of batch as columns.
+-- 2. Create a table for teachers with name, domain, department as column
+-- 3. Write a query to insert 10 students data and 10 teachers data in the respective table
+-- 4. Write a query to fetch all the students from 2020 batch
+-- 5. Write a query to fetch all teachers from CS department
+-- 6. Write a query to edit at least 3 records of students
+-- 7. Write a query to delete 2 records from teachers table.
+
